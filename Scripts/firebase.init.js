0d226c8eec403e3b67b724e775e4051f4a3bba0c
manikandannnn <77604076+manikandannnn@users.noreply.github.com>
2021-01-18 15:35:50 +0530

    script

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿var firebaseConfig = {
+    apiKey: "AIzaSyALTVtHfBQfyG76tR7azNqoOLG2pggi8Po",
+    authDomain: "chatapp-254e2.firebaseapp.com",
+    databaseURL: "https://chatapp-254e2-default-rtdb.firebaseio.com",
+    projectId: "chatapp-254e2",
+    storageBucket: "chatapp-254e2.appspot.com",
+    messagingSenderId: "892687806116",
+    appId: "1:892687806116:web:3f4029510eb7952e6b8031",
+    measurementId: "G-Q6MJLNC5GQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
